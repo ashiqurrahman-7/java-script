cc607875bbefe26c3ecd5d25a8295f239d9c7491
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-01-22 22:18:51 +0600

    data types: number,string and boolean

--- first.js
+++ first.js
@@ -3,8 +3,14 @@ var age =24;
 var year = 2000;
 
 console.log("weight:",weight);
+console.log(typeof weight)
 
 
 
 var  name = "Ashiqur rahman Tushar"
 console.log("namw:",name);
+console.log(typeof name);
+
+var isRich =true;
+console.log(isRich);
+console.log(typeof isRich);
