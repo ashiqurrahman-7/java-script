e156a4097fbdba354f1c7333fe7a480ec3e85e3c
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-01-23 23:33:21 +0600

    more comparision operation

--- comparsion.js
+++ comparsion.js
@@ -5,3 +5,14 @@ console.log( 30>5);
 console.log( 10 == 20);
 
 console.log( 20 != 10);
+
+console.log( 5>=10);
+console.log( 5 <=10);
+
+// output will be 
+// false
+// true
+// false
+// true
+// false
+// true
