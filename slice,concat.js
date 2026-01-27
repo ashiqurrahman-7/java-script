e5d720e9840c843e908bb69d7273bd3490d93010
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-01-27 21:56:42 +0600

    here i use java script spiliting

--- slice,concat.js
+++ slice,concat.js
@@ -1,3 +1,9 @@
-const name1 = 'AshiqurRahman';
-const part = name1.slice(2,4);
-console.log(part);
+// const name1 = 'AshiqurRahman';
+// const part = name1.slice(2,4);
+// console.log(part);
+
+
+
+
+const sentence = 'I am ashiqur rahman tushar';
+console.log(sentence.split(' '));
