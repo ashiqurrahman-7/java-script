81ff4af41f502a3f67117d0a03e74620762a17d1
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-01-27 21:41:04 +0600

    white space remove from string

--- case.js
+++ case.js
@@ -7,12 +7,27 @@
 
 
 
-const email1 = 'rahmanashiqir9@gmail.com';
-const email2 = 'RAhmanAshiqir9@Gmail.com';
+// const email1 = 'rahmanashiqir9@gmail.com';
+// const email2 = 'RAhmanAshiqir9@Gmail.com';
 
-if(email1.toLowerCase === email2.toLowerCase){
+// if(email1.toLowerCase === email2.toLowerCase){
+//     console.log("email verification successfully");
+// }
+// else{
+//     console.log("verification unsuccessfull");
+// }
+
+
+
+
+
+
+
+const password1 = ' water';  // has a double space in the middle
+const password2 = 'water';    // normal string
+
+if(password1.trim() === password2.trim()){
     console.log("email verification successfully");
-}
-else{
-    console.log("verification unsuccessfull");
+} else {
+    console.log("verification unsuccessful");
 }
