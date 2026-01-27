12a7a140f1f3db6b1c6e323f4c5120af20435fe6
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-01-27 21:30:10 +0600

    ebhave toLowerCase er maadhome string check kora hoi email verification er jonno

--- case.js
+++ case.js
@@ -1,5 +1,18 @@
-const name1 = "AShiqur Rahman TUShar";
+// const name1 = "AShiqur Rahman TUShar";
 
-console.log(name1);
-console.log(name1.toUpperCase());
-console.log(name1.toLowerCase());
+// console.log(name1);
+// console.log(name1.toUpperCase());
+// console.log(name1.toLowerCase());
+
+
+
+
+const email1 = 'rahmanashiqir9@gmail.com';
+const email2 = 'RAhmanAshiqir9@Gmail.com';
+
+if(email1.toLowerCase === email2.toLowerCase){
+    console.log("email verification successfully");
+}
+else{
+    console.log("verification unsuccessfull");
+}
