856ac0a138c1f3999ff019d15452942531633a24
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-01-27 20:52:22 +0600

    learn more string and object

--- string.js
+++ string.js
@@ -3,7 +3,12 @@ const division = "Dhaka";
 const district = `Gazipur`;
 const thana = new String('kashimpur'); 
 
-console.log(typeof country); 
-console.log(typeof division); 
-console.log(typeof district); 
-console.log(typeof thana); 
+// console.log(typeof country); 
+// console.log(typeof division); 
+// console.log(typeof district); 
+// console.log(typeof thana); 
+
+const capital = 'Ashiqur Rahman';
+console.log(capital);
+console.log(capital.length);
+console.log(capital[2]);
