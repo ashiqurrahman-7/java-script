d9a2b8e1305f6f3621b837d8874b2b159bc244e0
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-01-27 21:03:07 +0600

    learn more string and array that array is mutable but string is not mutable

--- string.js
+++ string.js
@@ -1,14 +1,28 @@
-const country = 'Bangladesh';
-const division = "Dhaka";
-const district = `Gazipur`;
-const thana = new String('kashimpur'); 
+// const country = 'Bangladesh';
+// const division = "Dhaka";
+// const district = `Gazipur`;
+// const thana = new String('kashimpur'); 
 
 // console.log(typeof country); 
 // console.log(typeof division); 
 // console.log(typeof district); 
 // console.log(typeof thana); 
 
-const capital = 'Ashiqur Rahman';
+// const capital = 'Ashiqur Rahman';
+// console.log(capital);
+// console.log(capital.length);
+// console.log(capital[2]);
+
+
+const numbers = [11,14,12,17,20,45,79];
+console.log(numbers);
+
+numbers[3] = 4444;
+console.log(numbers);
+
+// array mutable but string immutable ...
+const capital = 'Dhaka';
 console.log(capital);
-console.log(capital.length);
-console.log(capital[2]);
+
+capital[0] ='F';
+console.log(capital);
