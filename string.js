8bba19e31cf354cf97c1aa8033f2893ab1d62b57
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-01-27 20:39:34 +0600

    try to learn javascript string and object

---
+++ string.js
@@ -0,0 +1,9 @@
+const country = 'Bangladesh';
+const division = "Dhaka";
+const district = `Gazipur`;
+const thana = new String('kashimpur'); 
+
+console.log(typeof country); 
+console.log(typeof division); 
+console.log(typeof district); 
+console.log(typeof thana); 
