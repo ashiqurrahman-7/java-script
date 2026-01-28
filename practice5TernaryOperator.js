7dd6036dc869fe5cc43a3b43ecc30ad116bd9edb
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-01-28 19:39:29 +0600

    here i try to practice ternary operator

---
+++ practice5TernaryOperator.js
@@ -0,0 +1,12 @@
+// const age = 17;
+// if(age>=18){
+//     console.log("you can vote");
+// }
+// else{
+//     console.log("you cannot vote");
+// }
+
+//oporer kaaj amra ternary operator use koreo korte pari 
+
+const age = 18;
+age>=18 ? console.log("you can vote") : console.log("you cannot vote");
