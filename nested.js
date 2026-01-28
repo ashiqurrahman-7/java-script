6198aff7d6bf33257661435ed5bcc72ffd4be792
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-01-28 10:23:13 +0600

    practicing java script nested object

---
+++ nested.js
@@ -0,0 +1,16 @@
+const college = {
+    name:'abc',
+    class:['11','12'],
+    unique:{
+        color:'blue' ,
+        result:{
+            gpa:5,
+            merit:'top'
+        }
+    }
+};
+
+console.log(college.name);
+console.log(college.unique);
+console.log(college.unique.color);
+console.log(college.unique.result.merit);
