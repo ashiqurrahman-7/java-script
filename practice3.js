e849e551d0f2937a241b4f51cb792505c4c9da96
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-01-28 19:22:28 +0600

    here i try to practice comparison operator

---
+++ practice3.js
@@ -0,0 +1,8 @@
+console.log(10 ==10);  //true
+console.log(10 =='10'); //true ..here only check value
+console.log(10 === '10');  //false .here data type also checked
+
+
+console.log(10 !=10); //false ..10 10 are equal by value
+console.log(10!='10'); //false ...only check value
+console.log(10!=='10');
