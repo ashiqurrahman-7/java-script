fdddd9bf4385dbd2531a8d0062c507ea097c9a1a
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-01-28 11:23:11 +0600

    object to aray array to object

--- loop.js
+++ loop.js
@@ -6,7 +6,15 @@ const mobile ={
 }
 // console.log(mobile);
 
-for(const prop in mobile){
-    console.log(prop);
-    console.log(mobile[prop]);
+// for(const prop in mobile){
+//     console.log(prop);
+//     console.log(mobile[prop]);
+// }
+
+
+const  keys = Object.keys(mobile);
+console.log(keys);
+
+for(const key of keys){
+    console.log(key,' ',mobile[key]);
 }
