84c7c609471843c81b23b213b45931d8f0c35d0d
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-01-28 19:34:39 +0600

    try to practice a simple nested if else condition

---
+++ practice4IfElse.js
@@ -0,0 +1,14 @@
+const money = 420; //You are rich
+// const money =200 ;You are middle 
+// const money = 20; you are Goribs!
+if (money>300){
+    console.log("You are rich");
+}
+else{
+    if(money>100){
+        console.log("You are middle");
+    }
+    else{
+        console.log("you are Goribs!")
+    }
+}
