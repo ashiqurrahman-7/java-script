a295087efe1c420697d969690d75d3dacdbd6a6a
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-02-04 09:45:44 +0600

    printing array ,find out length,push and pop element in array

---
+++ arrays1.js
@@ -0,0 +1,25 @@
+let numbers = [ 10,2,30,20,45,60,43,50,90,70];
+console.log(numbers);
+
+console.log(numbers.length);
+console.log(numbers[2]);
+
+
+//push pop technique
+
+numbers.push(99);
+console.log(numbers);
+
+const out1 = numbers.pop();
+console.log(out1);
+
+console.log(numbers);
+
+
+
+const out2 = numbers.pop();
+console.log(out2);
+
+console.log(numbers);
+
+console.log(out1, out2);
