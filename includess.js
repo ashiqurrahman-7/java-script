8b54f24c3eb8b6f05b6aa423d076e20756cd1d96
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-02-04 10:00:55 +0600

    to check is it an array or not using (Array.isArray(arrayName))

--- includess.js
+++ includess.js
@@ -13,3 +13,11 @@ else{
 
 console.log(players.indexOf('Neyman')); // if it not found in array it will show  -1 
 console.log(players.indexOf('neymar'));  //3
+
+//to check is it an array or not
+
+console.log(Array.isArray(players));
+
+
+const food = 'papaya, banana, apple';
+console.log(Array.isArray(food));  //food is not an array ..It is a  string 
