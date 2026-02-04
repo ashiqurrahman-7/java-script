994047904eaf5828ac6fa33b3b303a7b638bd48a
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-02-04 09:28:58 +0600

    add two number and fixed decimal value what i need .And check datatype after using toFixed()

--- practice6.js
+++ practice6.js
@@ -1,18 +1,32 @@
-var price1 = 25.50;
-var price2 = 250;
+// var price1 = 25.50;
+// var price2 = 250;
 
 
-console.log(price1);
-console.log(price2);
+// console.log(price1);
+// console.log(price2);
 
-console.log('------------------------------------');
+// console.log('------------------------------------');
 
-console.log( typeof(price1));
-console.log( typeof(price2));
+// console.log( typeof(price1));
+// console.log( typeof(price2));
 
-console.log('-------------------------------------');
+// console.log('-------------------------------------');
 
-var prices1 = parseInt(price1);
-console.log(prices1);
-var prices2 = parseFloat(price2);
-console.log(prices2);
+// var prices1 = parseInt(price1);
+// console.log(prices1);
+// var prices2 = parseFloat(price2);
+// console.log(prices2);
+
+
+
+
+
+
+var first = 0.1;
+var second = 0.2;
+
+var total = first +  second;
+console.log( total);
+
+console.log(typeof total.toFixed(3));
+console.log(typeof total);
