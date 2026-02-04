670ea44a68f4ac29f129201f1df360b9c6525108
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-02-04 10:17:40 +0600

    practicing join(),concat(),slice()operation

---
+++ joinConcatSliceOperation.js
@@ -0,0 +1,23 @@
+// //join operaion  
+
+// const apps = [ 'insta' ,'fb', 'twitter','wechat','messenger','telegram'];
+// console.log(apps);
+// console.log('-------------------------------');
+// apps.join(); //join er moddhe kono symbol na dile koma dia dia dekhabe 
+// console.log(apps);
+// console.log(apps.join(' + '));
+
+
+// console.log('```````````````````````````````````````````````````````````````````````````````````````````````````````````````');
+// //concat operation
+
+// const data1 = ['a','b','c','d','e'];
+// const data2 = ['f','g','h','i','j'];
+// console.log(data1.concat(data2));
+
+
+
+console.log('```````````````````````````````````````````````````````````````````````````````````````````````````````````````');
+const letters = ['a','b','c','d','e','f','g','h','i','j'];
+console.log(letters);
+console.log(letters.slice(1,5));
