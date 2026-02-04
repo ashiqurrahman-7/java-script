f45cf8fd9c70b48608bc67b52b8aee8db2090987
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-02-04 11:08:33 +0600

    practicing basic while loop

---
+++ whileLoop.js
@@ -0,0 +1,18 @@
+var num = 0;
+while(num<5){
+    console.log(num);
+    num++;
+}
+
+console.log('------------------------------------------------------------------')
+//another example of while loop
+
+let nums =1;
+let sum =0;
+
+while(nums<=10){
+    console.log(nums);
+    sum = sum + nums ;
+    nums++;
+}
+  console.log('sum is :',sum);
