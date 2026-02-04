628c276df043301b02c86ad755a0f35704e057b1
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-02-04 09:14:33 +0600

    practicing number convertion and check type of number

---
+++ practice6.js
@@ -0,0 +1,18 @@
+var price1 = 25.50;
+var price2 = 250;
+
+
+console.log(price1);
+console.log(price2);
+
+console.log('------------------------------------');
+
+console.log( typeof(price1));
+console.log( typeof(price2));
+
+console.log('-------------------------------------');
+
+var prices1 = parseInt(price1);
+console.log(prices1);
+var prices2 = parseFloat(price2);
+console.log(prices2);
