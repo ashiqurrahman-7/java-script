36e12da0c022c0f84816813f97ab91022567e021
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-02-05 09:34:34 +0600

    try to practicing split and slice

---
+++ sliceJoinSplitIncludes.js
@@ -0,0 +1,9 @@
+const name1 = 'Ashiqur Rahman';
+const part  = name1.slice(2,5); //means start from 2 index and first before 5
+console.log(part);
+
+
+const sentence = "This is Ashiqur Rahman Tushar";
+console.log(sentence.split()); //This will do nothing exactly show the given sentence
+console.log(sentence.split(''));//This will show the sentence in every letter and comma symbol
+console.log(sentence.split(' ')); //This will show spliting every word  .basically it checks white space and make them different string
