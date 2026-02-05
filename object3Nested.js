448ee0e797a0c0875d2c6c61b84990c53b6e13e5
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-02-05 18:50:45 +0600

    get element from nested object and delete them

---
+++ object3Nested.js
@@ -0,0 +1,24 @@
+const college ={
+    name: 'abc',
+    class:[11,12],
+    events:['science fair', 'victory day', '21Feb'],
+        unique:{
+            color:'blue',
+            result:{
+                gpa:5,
+                merit:'top'
+            }
+        }
+}
+
+// console.log(college);
+// console.log(college.name); //abc
+
+// console.log(college.unique);
+// console.log(college.unique.color); //blue
+// console.log(college.unique.result.merit);  //top
+
+
+//deleting from object
+delete college.unique.result.merit;
+console.log(college);
