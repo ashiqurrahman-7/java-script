32d9be7144e4a509a9e0fedc0fd0cd4957ba723d
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-02-05 10:06:45 +0600

    get key and value info from object

---
+++ object2.js
@@ -0,0 +1,13 @@
+const computer ={
+    brand:'lenevo',
+    price : 65000,
+    processor: 'intel-i5',
+    ssd: '1TB',
+    monitor:'hp'
+}
+const keys = Object.keys(computer);
+console.log(keys);
+
+
+const values = Object.values(computer);
+console.log(values);
