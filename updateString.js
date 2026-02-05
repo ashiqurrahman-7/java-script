f8169f88ecea1ca9c833d0fc06b776ab09f72d09
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-02-05 09:13:59 +0600

    try to update array and string...Array is mutable but string is unmutable..String can not be changed

---
+++ updateString.js
@@ -0,0 +1,19 @@
+
+//array is mutable..so the value will be changed..
+const numbers = [11,13,14,16,18,50,30,20];
+console.log(numbers);
+
+numbers[3] = 66;
+console.log(numbers);
+
+
+
+
+
+
+//String is unmutable ...So , it can not be changed......
+let capital = 'Dhaka';
+console.log(capital);
+
+capital[0] = 'F';
+console.log(capital);
