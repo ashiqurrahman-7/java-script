6ebd2186f3405fe66a8d09665260de77cc75214a
ashiqurrahman-7 <ashiqurrahmantusher007@gmail.com>
2026-02-05 19:30:36 +0600

    get element from array using for of,for and while loop

---
+++ array.js
@@ -0,0 +1,22 @@
+const friends = ['a','b','c','d','e'];
+
+
+//susing for of loop to  get the element from the array
+// for(const friend of friends){
+//     console.log(friend);
+// }
+
+
+//using for loop to get the element from the array
+// for(let i = 0; i< friends.length ; i++){
+//     // console.log(i);
+//     console.log(friends[i]);
+// }
+
+
+let i =0;
+while(i<friends.length){
+    
+    i++;
+}
+console.log(friends);
